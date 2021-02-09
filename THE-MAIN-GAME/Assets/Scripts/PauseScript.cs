999528e331ae8f2a01c3caa93a7f786f768f33e2
notalant312 <arturikoks14@gmail.com>
2021-02-09 20:18:15 +0300

    pause added
    press escape. And if pause active time scale is zero

---
+++ THE-MAIN-GAME/Assets/Scripts/PauseScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseScript : MonoBehaviour
+{
+    public GameObject PausePan;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PausePan.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+    
+    public void OnClickExit()
+    {
+        PausePan.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
