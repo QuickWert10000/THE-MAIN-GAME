677c10229e20a769e1b6da38aa6d00d65e48eecc
notalant312 <arturikoks14@gmail.com>
2021-02-10 18:11:10 +0300

    graphic upd.
    added health bar, fixed some bugs, added first power, added slots

---
+++ THE-MAIN-GAME/Assets/Scripts/Power.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Power : MonoBehaviour
+{
+    public float speed;
+    public float lifetime;
+    public float distance;
+    public int damage;
+    public LayerMask whatIsSolid;
+    bool isRight;
+
+    private void Start()
+    {
+        isRight = GameObject.Find("Player").GetComponent<PlayerMovement>().facingRight;
+    }
+
+    private void Update()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.up, distance, whatIsSolid);
+        if (hitInfo.collider != null)
+        {
+            if (hitInfo.collider.CompareTag("Enemy"))
+            {
+                hitInfo.collider.GetComponent<Enemy>().TakeDamage(damage);
+            }
+            Destroy(gameObject);
+        }
+        if (isRight == true)
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+
+    }
+
+}
