a5cf1393ecac790e34cca5d727823377276fa997
QuickWert10000 <77354872+QuickWert10000@users.noreply.github.com>
2021-02-11 14:34:11 +0300

    Enemy
    -Added enemy following feature
    -Changed camera ratio to 16:9

--- THE-MAIN-GAME/Assets/Scripts/Enemy.cs
+++ THE-MAIN-GAME/Assets/Scripts/Enemy.cs
@@ -2,22 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
-{
-    public int health;
-    public float speed;
+public class Enemy : MonoBehaviour{
+    public int health =10;
+    public float speed =2f;
+    public Transform player;
 
-    public void Update()
-    {
+    private Vector2 lastPosition;
+    private Vector2 newPosition;
+
+// The enemy object's movement
+    void Update(){
+	lastPosition = transform.position;
+// Move towards player
+	if (Vector2.Distance(player.position, transform.position)<5 && Vector2.Distance(player.position, transform.position)>1){
+	    transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+    }
+
+	newPosition = transform.position;
+// Death
         if (health <= 0)
         {
             Destroy(gameObject);
-        }
-        transform.Translate(Vector2.left * speed * Time.deltaTime);
     }
+
+// Flipping the enemy
+	if (lastPosition.x < newPosition.x){
+	    flipRight();
+    }   else if (lastPosition.x > newPosition.x){
+	    flipLeft();
+    }
+
+    }
+
+// The enemy object takes damage
     public void TakeDamage(int damage)
     {
         health -= damage;
     }
 
+// The flipper functions
+    void flipRight(){
+	    Vector3 Scaler = transform.localScale;
+	    if (Scaler.x > 0){    
+	        Scaler.x *= -1;
+	        transform.localScale = Scaler;
+        }
+    }
+
+    void flipLeft(){
+	    Vector3 Scaler = transform.localScale;
+	    if (Scaler.x < 0){    
+	        Scaler.x *= -1;
+	        transform.localScale = Scaler;
+        }
+        }
 }
+
